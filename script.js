a9a94f4811f468e3c6e19304a9fba08ccf76a358
pratik <pratikbhattarai64@gmail.com>
2022-01-31 14:10:16 +0545

    commit-all

---
+++ script.js
@@ -0,0 +1,32 @@
+let inputField=document.getElementById('inputField');
+let inputresult=document.getElementById('inputresult');
+
+
+
+
+function add (result){
+   calculatorForm.evalresult.value=calculatorForm.evalresult.
+   value + result;
+}
+
+function AC (){
+inputField.value=''    
+
+}
+function square() {
+ 
+   inputField.value=
+   inputField.value*inputField.value
+
+
+}
+
+
+function add4(){
+   inputField.value=inputField.value*inputField.value*inputField.value
+}
+
+
+
+
+
